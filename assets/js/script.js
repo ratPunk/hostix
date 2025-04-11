347d6135e6fb2314889312b97a0ac57e0486d5c0
AuitDarkness <zipperyt.z@gmail.com>
2025-04-11 22:18:05 +0300

    update structures files

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+// Обновление значений ползунков
+document.getElementById('cpu-slider').addEventListener('input', function() {
+    const cpuValue = this.value;
+    document.getElementById('cpu-value').textContent = `${cpuValue} ядра`;
+    updateTotalPrice();
+});
+
+document.getElementById('ram-slider').addEventListener('input', function() {
+    const ramValue = this.value;
+    document.getElementById('ram-value').textContent = `${ramValue} ГБ`;
+    updateTotalPrice();
+});
+
+document.getElementById('disk-slider').addEventListener('input', function() {
+    const diskOptions = [
+        '15 ГБ',  
+        '35 ГБ',
+        '50 ГБ',  
+        '80 ГБ',  
+        '100 ГБ',  
+
+    ];
+    document.getElementById('disk-value').textContent = diskOptions[this.value - 1];
+    updateTotalPrice();
+});
+
+document.getElementById('network-slider').addEventListener('input', function() {
+    const networkOptions = [
+            '50 Мбит/с',
+            '100 Мбит/с',
+            '150 Мбит/с'
+        ];
+    document.getElementById('network-value').textContent = networkOptions[this.value - 1];
+    updateTotalPrice();
+});
+
+// Расчет итоговой ценыыы
+function updateTotalPrice() {
+    const cpuPrice = parseInt(document.getElementById('cpu-slider').value) * 25;
+    const ramPrice = parseInt(document.getElementById('ram-slider').value) * 10;
+    const diskPrice = parseInt(document.getElementById('disk-slider').value) * 50;
+    const networkPrice = parseInt(document.getElementById('network-slider').value) * 30;
+    
+    const total = cpuPrice + ramPrice + diskPrice + networkPrice;
+    document.getElementById('total-price').textContent = `${total}₽/час`;
+}
+
